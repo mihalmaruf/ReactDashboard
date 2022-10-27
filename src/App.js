6611e53d14c0fef368876553a928c8ebd3a61e38
Mihal <marufmihal43@gmail.com>
2022-10-27 10:27:24 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import List from './pages/list/List'
+import Login from './pages/login/Login'
+import New from './pages/new/New'
+import Single from './pages/single/Single'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/users' element={<List />} />
+        <Route path='/users/:userId' element={<Single />} />
+        <Route path='/users/new' element={<New />} />
+        <Route path='/products' element={<List />} />
+        <Route path='/products/:productId' element={<Single />} />
+        <Route path='/products/new' element={<New />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
